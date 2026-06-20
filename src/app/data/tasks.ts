3143c2e92f8abeeb1ef2b45f27c58ebeecc1b909
Gabriel Tavares dos Santos <tavares.gabriel.dev@gmail.com>
2026-06-20 18:24:16 -0300

    Refactor Home page to display tasks from a new data source. Added tasks data and type definitions for better structure and maintainability.

---
+++ src/app/data/tasks.ts
@@ -0,0 +1,32 @@
+import Task from "../types/tasks";
+const tasks: Task[] = [
+  {
+    id: 1,
+    title: "Estudar TypeScript",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Praticar métodos de array",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Fazer exercício de map",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Revisar reduce",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "Criar projeto pessoal",
+    completed: true,
+  },
+];
+
+console.log(tasks);
+
+export default tasks;
